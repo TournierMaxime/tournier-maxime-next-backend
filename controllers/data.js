1e69c928431f8d52c033e9772c74eddbcc69f325
Hoggy06 <speedrun.fra@gmail.com>
2022-08-13 21:06:04 +0200

    ajout des controllers

---
+++ controllers/data.js
@@ -0,0 +1,20 @@
+const { Data } = require("../models/Data.js");
+//Récupère tous les posts
+exports.getAllData = (req, res, next) => {
+  if (req.method === "GET") {
+    res.status(200).json(Data);
+  }
+};
+
+exports.getOneData = (req, res, next) => {
+  const id = Data[1].realisation.projects.find(
+    (project) => project.id === Number(req.params.id)
+  );
+  if (req.method === "GET") {
+    if (id) {
+      return res.status(200).json(id);
+    } else {
+      return res.status(404).json({ message: "Donnée non trouvée" });
+    }
+  }
+};
